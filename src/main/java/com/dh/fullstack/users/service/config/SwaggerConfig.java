403b30588683aae5c7bef302eccc8b4ac5a31c24
Santiago Mamani Zeballos <sm.zeballos.umss@gmail.com>
2019-07-17 08:54:27 -0400

    add information to swagger

--- src/main/java/com/dh/fullstack/users/service/config/SwaggerConfig.java
+++ src/main/java/com/dh/fullstack/users/service/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.dh.fullstack.users.service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,6 +25,18 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors
                         .basePackage("com.dh.fullstack.users.service.controller"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+
+        return new ApiInfoBuilder()
+                .title("Users Service API")
+                .description("Users Management REST API ")
+                .contact(new Contact("santiago", "", "sm.zeballos.umss@gmail.com"))
+                .version("0.0.1")
+                .license("Apache 1.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
                 .build();
     }
 }
