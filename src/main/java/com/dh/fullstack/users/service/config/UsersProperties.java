797a0d3a35e9f16c4a92020c15cd113c9fd97da9
Santiago Mamani Zeballos <sm.zeballos.umss@gmail.com>
2019-07-19 08:03:54 -0400

    read file properties

---
+++ src/main/java/com/dh/fullstack/users/service/config/UsersProperties.java
@@ -0,0 +1,18 @@
+package com.dh.fullstack.users.service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Santiago Mamani
+ */
+@Configuration
+public class UsersProperties {
+
+    @Value("${users.permit.age}")
+    private Integer permitAge;
+
+    public Integer getPermitAge() {
+        return permitAge;
+    }
+}
--- src/main/java/com/dh/fullstack/users/service/model/repositories/ContractRepository.java
+++ src/main/java/com/dh/fullstack/users/service/model/repositories/ContractRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Santiago Mamani
  */
 public interface ContractRepository extends JpaRepository<Contract,Long> {
+
 }
